a6cd90344508308d0ab1863a82b295a8d25865fa
NelsonFlores1 <49950492+NelsonFlores1@users.noreply.github.com>
2026-05-26 19:44:10 -0400

    prueba 11

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -10,8 +10,16 @@ export default defineConfig({
     ['list'],
     ['html', { open: 'never', outputFolder: 'playwright-report' }],
   ],
+  webServer: { // para evitar el error de que el frontend no se levanta
+    command: 'npm run dev -- --host 0.0.0.0 --port 5173',
+    url: 'http://127.0.0.1:5173',
+    // reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
+    cwd: '../frontend',
+    timeout: 120000,
+  },
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://127.0.0.1:5173',
     headless: true,
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure',
